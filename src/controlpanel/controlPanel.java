8a7743b7f7f3004a779734c5c7c1c58dad344ae0
Houley <ethouley@gmail.com>
2020-03-28 23:27:44 +1000

    Control Panel Buttons (crappy GUI ) and login screen
    Control Panel which contains a buttons for each sections of the program.
    Need to design the look and feel of the Control Panel.
    Need to clean the code up to match and possible move to another class
    
    Login window read for a backend connection.
    Needing hashing of password, comparing password, setting users access level.

--- src/controlpanel/controlPanel.java
+++ src/controlpanel/controlPanel.java
@@ -1,7 +1,40 @@
 package controlpanel;
 
+import javax.swing.*;
+import java.awt.*;
+
+
 public class controlPanel {
+    JFrame mainControlPanelScreen = new JFrame(); // creating instance of JFrame
+    controlPanel() {
+        JButton createBillboardsButton = new JButton("Create Billboards");
+        JButton listBillboardsButton = new JButton("List Billboards");
+        JButton scheduleBillboardsButton = new JButton("Schedule Billboards");
+        JButton editUserButton = new JButton("Edit User's");
+        JButton logOutButton = new JButton("Logout");
+
+        mainControlPanelScreen.add(createBillboardsButton);
+        mainControlPanelScreen.add(listBillboardsButton);
+        mainControlPanelScreen.add(scheduleBillboardsButton);
+        mainControlPanelScreen.add(editUserButton);
+        mainControlPanelScreen.add(logOutButton);
+
+
+//      Grid Layout
+        mainControlPanelScreen.setLayout(new GridLayout(3, 2));
+        mainControlPanelScreen.setSize(500,500);
+        mainControlPanelScreen.setVisible(true);
+
+        // Exit program cleanly
+        mainControlPanelScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args) {
+
+        login loginScreen = new login();
+        loginScreen.setSize(300,100);
+        loginScreen.setVisible(true);
+        loginScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
