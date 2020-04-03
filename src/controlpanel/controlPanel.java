18bba0b4f2fd71330fd14f9b7117ca4213d6c10e
Houley <ethouley@gmail.com>
2020-04-03 15:56:53 +1000

    Login screen designed, Used MDI as the style program (Fixed Upstream data)
    Add files:
    src/controlpanel/controller.java
    
    Removed files:
    src/controlpanel/placeholder.txt
    .idea/workspace.xml
    
    Changed files:
    src/controlpanel/login.java
    src/controlpanel/controlPanel.java

--- src/controlpanel/controlPanel.java
+++ src/controlpanel/controlPanel.java
@@ -4,33 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 
 
-<<<<<<< Updated upstream
-public class controlPanel {
-    JFrame mainControlPanelScreen = new JFrame(); // creating instance of JFrame
-    controlPanel() {
-        JButton createBillboardsButton = new JButton("Create Billboards");
-        JButton listBillboardsButton = new JButton("List Billboards");
-        JButton scheduleBillboardsButton = new JButton("Schedule Billboards");
-        JButton editUserButton = new JButton("Edit User's");
-        JButton logOutButton = new JButton("Logout");
-
-        mainControlPanelScreen.add(createBillboardsButton);
-        mainControlPanelScreen.add(listBillboardsButton);
-        mainControlPanelScreen.add(scheduleBillboardsButton);
-        mainControlPanelScreen.add(editUserButton);
-        mainControlPanelScreen.add(logOutButton);
-
-
-//      Grid Layout
-        mainControlPanelScreen.setLayout(new GridLayout(3, 2));
-        mainControlPanelScreen.setSize(500,500);
-        mainControlPanelScreen.setVisible(true);
-
-        // Exit program cleanly
-        mainControlPanelScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-=======
 public class controlPanel extends JFrame {
 
 //    private static final boolean CLOSABLE = true;
@@ -67,18 +40,10 @@ public class controlPanel extends JFrame {
         setVisible(true);
     }
 
-
-
-
->>>>>>> Stashed changes
     public static void main(String[] args) {
+//        Create and instance of the program
         controlPanel program = new controlPanel();
+//        Allow the program to be seen.
         program.setVisible(true);
-
-        login loginScreen = new login();
-        loginScreen.setSize(300,100);
-        loginScreen.setVisible(true);
-        loginScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
 }
--- src/controlpanel/login.java
+++ src/controlpanel/login.java
@@ -3,67 +3,6 @@ package controlpanel;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-<<<<<<< Updated upstream
-class login extends JFrame implements ActionListener
-{
-    JButton loginButton;
-    JPanel loginWindow;
-    JLabel usernameLabel, passwordLabel;
-    final JTextField  usernameText, passwordText;
-    login()
-    {
-//       Username
-        usernameLabel = new JLabel("Username:");
-        usernameText = new JTextField(15);
-//      Password
-        passwordLabel = new JLabel("Password:");
-        passwordText = new JPasswordField(15); // Hides the Password from the screen
-
-
-        loginButton = new JButton("Login");
-//      Set window to Grid layout
-        loginWindow = new JPanel(new GridLayout(3,2));
-//      Add items to the window
-        loginWindow.add(usernameLabel);
-        loginWindow.add(usernameText);
-        loginWindow.add(passwordLabel);
-        loginWindow.add(passwordText);
-        loginWindow.add(loginButton);
-        add(loginWindow,BorderLayout.CENTER);
-        loginButton.addActionListener(this);
-        setTitle("Login Screen");
-    }
-
-
-    public void actionPerformed(ActionEvent e) {
-
-        String uname = usernameText.getText();
-        String pass = passwordText.getText();
-
-        // TEMP FUNCTIONS WILL BE REMOVED and replaced with one from the backend functions
-        if (uname.equals("john1") && pass.equals("Password.1")) {
-//            Set user access level
-            JOptionPane.showMessageDialog(this, "Successful Login",
-                    "Successful", JOptionPane.INFORMATION_MESSAGE);
-        } else {
-            JOptionPane.showMessageDialog(this, "Incorrect Login Informations",
-                    "Error", JOptionPane.ERROR_MESSAGE);
-            System.out.println(uname);
-            System.out.println(pass);
-
-        }
-
-//      REAL FUNCTION with temp place Holders
-        /*      Temp function for sending the username and password to the server application */
-//        if (sendUsernameAndpassword(uname, hashfunction(pass).equals(true))) {
-//            Set user access level
-//        } else {
-//            JOptionPane.showMessageDialog(this, "Incorrect Login Informations",
-//                    "Error", JOptionPane.ERROR_MESSAGE);
-//        }
-
-
-=======
 
 class login extends JFrame
 {
@@ -114,7 +53,6 @@ class login extends JFrame
             }
         });
         return window;
->>>>>>> Stashed changes
 
     }
 }
