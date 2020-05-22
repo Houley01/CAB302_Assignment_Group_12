cbb768fd0f5f622563021d318a1209b045ebef2a
isabellabzatto <50016435+isabellabzatto@users.noreply.github.com>
2020-05-22 14:12:44 +1000

    Edit User Settings
    Finished individual GUI's for editing user settings

--- src/controlpanel/DialogWindow.java
+++ src/controlpanel/DialogWindow.java
@@ -1,10 +1,11 @@
 package controlpanel;
 
+import javax.naming.spi.ObjectFactoryBuilder;
 import javax.swing.*;
 import java.awt.*;
 
 public class DialogWindow {
-//    Used to create dialog windows without causing a bug which stops from reading some text fields
+    //    Used to create dialog windows without causing a bug which stops from reading some text fields
     static protected void showErrorPane(String message, String title) {
         JOptionPane pane = new JOptionPane(message, JOptionPane.ERROR_MESSAGE);
         JDialog dialog = pane.createDialog(title);
@@ -33,10 +34,6 @@ public class DialogWindow {
         Object changePassword = JOptionPane.showInputDialog(editPassword, "Change your password:");
         System.out.println(changePassword);
     }
-//        JOptionPane pane = new JOptionPane("Please Change Password", JOptionPane.WA);
-//        JDialog dialog = pane.createDialog("Change Password");
-//        dialog.setAlwaysOnTop(true);
-//        dialog.setVisible(true);
 
     // User Permissions
     static void showUserPermissions() {
@@ -48,30 +45,55 @@ public class DialogWindow {
         JCheckBox schedulePermissions = new JCheckBox("Schedule Billboards");
         JCheckBox editUserPermissions = new JCheckBox("Edit User Permissions");
 
-
-        pane.setLayout(new GridLayout(2,1));
-        window2.setLayout(new GridLayout(4,1));
-
+        pane.setLayout(new GridLayout(1,1));
+        pane.add(window2);
+        window2.setLayout(new GridLayout(2,2));
         window2.add(billboardPermissions);
         window2.add(editBillboardPermissions);
         window2.add(schedulePermissions);
         window2.add(editUserPermissions);
 
-        pane.add(window2);
-
         dialog.setAlwaysOnTop(true);
         dialog.setVisible(true);
-
-
     }
 
     // User Details
+    static void showUserDetails() {
+        JFrame editFirstName = new JFrame();
+        Object changeFirstName = JOptionPane.showInputDialog(editFirstName, "Update First Name:");
+        JFrame editLastName = new JFrame();
+        Object changeLastName = JOptionPane.showInputDialog(editLastName, "Update Last Name:");
+        System.out.println(changeFirstName);
+        System.out.println(changeLastName);
+    }
 
     // Create User
+    static void showCreateUser() {
+        JFrame enterFirstName = new JFrame();
+        Object inputFirstName = JOptionPane.showInputDialog(enterFirstName, "Enter First Name:");
+        JFrame enterLastName = new JFrame();
+        Object inputLastName = JOptionPane.showInputDialog(enterLastName, "Enter Last Name:");
+        JFrame enterUserName = new JFrame();
+        Object inputUserName = JOptionPane.showInputDialog(enterUserName, "Enter UserName");
+        JFrame enterPassword = new JFrame();
+        Object inputPassword = JOptionPane.showInputDialog(enterPassword, "Enter Password");
+        String[] userPermissions = {"Create Billboards", "Edit Billboards", "Schedule Billboards", "Edit Users"};
+        String  permissions = (String) JOptionPane.showInputDialog(null, "Select User Permissions:",
+                "User Permissions", JOptionPane.QUESTION_MESSAGE, null,
+                userPermissions, userPermissions[1]);
+
+        System.out.println(inputFirstName);
+        System.out.println(inputLastName);
+        System.out.println(inputUserName);
+        System.out.println(inputPassword);
+        System.out.println(permissions);
+    }
 
     // Delete User
-
-
-
+    static void showRemoveUser() {
+        JFrame deleteUser = new JFrame();
+        String removeUser = JOptionPane.showInputDialog(deleteUser, "Delete a User:");
+        System.out.println(removeUser);
+    }
 
 }
